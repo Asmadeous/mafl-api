6736675ef0967104b3e8e124c9d89f3112f7ad65
Asmadeous <iansky3311@gmail.com>
2025-05-02 01:53:46 +0300

    FIX

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -1,5 +1,6 @@
 # app/controllers/health_controller.rb (or create a custom one)
 class HealthController < ApplicationController
+  respond_to :json
   skip_before_action :authenticate_user!
   def show
     Rails.logger.info "Health check request headers: #{request.headers.env.select { |k, _| k.start_with?('HTTP_') }}"
