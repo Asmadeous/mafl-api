291a6490dc467715a318ea70d206f535eea530e8
Asmadeous <iansky3311@gmail.com>
2025-05-19 22:42:32 +0300

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,8 +40,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   # Set allowed hosts
-  config.hosts << "mafl-api-production.up.railway.app"
-  config.hosts << "mafl-api-production.up.railway.app"
+  # config.hosts << "mafl-api-production.up.railway.app"
+  # config.hosts << "mafl-api-production.up.railway.app"
   config.hosts << "localhost"
 
   # Action Cable config
