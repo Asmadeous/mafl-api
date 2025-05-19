98c75bac8d54ac87f7c6b8700c9b48c0e6572616
Asmadeous <iansky3311@gmail.com>
2025-05-19 20:48:28 +0300

    fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins [  "https://www.mafllogistics.com" ]  # later change to the domain of the frontend app
+    origins [ "https://mafllogistics.com" ]  # later change to the domain of the frontend app
 
     resource "*",
       headers: :any,
